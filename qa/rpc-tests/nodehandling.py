6b7d88ae8bd59b7ead01289ded6a901134be483b
Za Wilcox <zancas@protonmail.com>
2019-07-20 11:21:49 -0600

    fix imports

--- qa/rpc-tests/nodehandling.py
+++ qa/rpc-tests/nodehandling.py
@@ -14,10 +14,7 @@ from test_framework.util import assert_equal, connect_nodes_bi, p2p_port
 
 import time
 
-try:
-    import urllib.parse as urlparse
-except ImportError:
-    import urllib.parse
+import urllib
 
 class NodeHandlingTest (BitcoinTestFramework):
     def run_test(self):

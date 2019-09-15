fcadb493817c8c79b9e92c35202281fbf73a94a5
Za Wilcox <zancas@protonmail.com>
2019-09-15 10:47:18 -0600

    add image_id 'community ubuntu disco minimal' ami-0006ea01070db4b0d

--- contrib/ci-workers/provisionec2/src/bin/provision.rs
+++ contrib/ci-workers/provisionec2/src/bin/provision.rs
@@ -1,31 +1,14 @@
 #![deny(unsafe_code)]
-//use structopt::StructOpt;
 use rusoto_ec2::{Ec2, Filter};
 fn main() {
-    //let cred_provide =
-    //    rusoto_credential::StaticProvider::new_minimal("foo".to_string(), "spam".to_string());
     let client = rusoto_ec2::Ec2Client::new(rusoto_core::Region::UsEast1);
-    let image_description_request = rusoto_ec2::DescribeImagesRequest {
-        dry_run: Some(false),
-        executable_users: Some(vec!["all".to_string()]),
-        filters: Some(vec![
-            Filter {
-                name: Some(String::from("architecture")),
-                values: Some(vec![String::from("x86_64")]),
-            },
-            Filter {
-                name: Some(String::from("state")),
-                values: Some(vec![String::from("available")]),
-            },
-        ]),
-        image_ids: None,
-        owners: Some(vec!["aws-marketplace".to_string()]),
+    let run_instance_request = rusoto_ec2::RunInstancesRequest {
+        dry_run: Some(true),
+        image_id: Some(String::from("ami-0006ea01070db4b0d")),
+        ..Default::default()
     };
     println!(
         "{:#?}",
-        client
-            .describe_images(image_description_request)
-            .sync()
-            .unwrap()
+        client.run_instances(run_instance_request).sync().unwrap()
     );
 }

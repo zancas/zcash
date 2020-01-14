15953995234dc13af142db2b15da943371325b1e
zancas <zancas@protonmail.com>
2020-01-14 16:30:15 -0700

    generate 101 blocks

--- qa/rpc-tests/intermittent_failures.py
+++ qa/rpc-tests/intermittent_failures.py
@@ -27,7 +27,7 @@ class WalletListNotes(BitcoinTestFramework):
         recipients = [{"address":saplingzaddr, "amount": receive_amount_10}]
         myopid = self.nodes[0].z_sendmany(get_coinbase_address(self.nodes[0]), recipients)
         txid_1 = wait_and_assert_operationid_status(self.nodes[0], myopid)
-        self.nodes[0].generate(1)
+        self.nodes[0].generate(101)
         self.sync_all()
 
         # Send 0.001 (actually 0.9988) from saplingzaddr to a new zaddr

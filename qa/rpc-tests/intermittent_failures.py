9bb9feebc9fe8dca85f511af429720a8a73ffc57
zancas <zancas@protonmail.com>
2020-01-14 16:19:50 -0700

    debug print

--- qa/rpc-tests/intermittent_failures.py
+++ qa/rpc-tests/intermittent_failures.py
@@ -45,6 +45,7 @@ class WalletListNotes(BitcoinTestFramework):
             # list unspent, allowing 0conf txs
             unspent_tx = self.nodes[0].z_listunspent(0)
             message = "The count at failure was: %s" % count
+            print(message)
             assert_equal(len(unspent_tx), 1 + count, message)
 
 

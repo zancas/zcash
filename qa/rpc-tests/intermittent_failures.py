7f7605d5e27e8ee792f00f952d1966831f7043ed
zancas <zancas@protonmail.com>
2020-01-14 15:12:40 -0700

    add receive_amount_10 back

--- qa/rpc-tests/intermittent_failures.py
+++ qa/rpc-tests/intermittent_failures.py
@@ -22,8 +22,9 @@ class WalletListNotes(BitcoinTestFramework):
         assert_equal(200, self.nodes[0].getblockcount())
         saplingzaddr = self.nodes[0].z_getnewaddress('sapling')
         saplingzaddr2 = self.nodes[0].z_getnewaddress('sapling')
+        receive_amount_10 = Decimal('10.0') - Decimal('0.0001')
 
-        # Send 1.0 (actually 0.9999) from sproutzaddr to a new zaddr
+        # Send 1.0 (actually 0.9999) from saplingzaddr to a new zaddr
         receive_amount_1 = Decimal('1.0') - Decimal('0.0001')
         change_amount_9 = receive_amount_10 - Decimal('1.0')
         assert_equal('sapling', self.nodes[0].z_validateaddress(saplingzaddr)['type'])
